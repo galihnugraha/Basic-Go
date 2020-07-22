aff707c877bc92fa3cf13f3045e0464feca31d05
galihnugraha <61906823+galihnugraha@users.noreply.github.com>
2020-07-22 20:31:59 +0700

    create itc.go
    membuat file dengan isi nama kakak dan nama adik (merosa pesona)

---
+++ itc.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var namaKakak = "Merosa Pesona"
+	var namaAdik = "Anosep Asorem"
+
+	fmt.Println("Nama Kakak : " + namaKakak)
+	fmt.Println("Nama Adik : " + namaAdik)
+}
